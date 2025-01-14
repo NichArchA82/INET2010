4b6de70f8dca4a941f9b83d5ed5c0b767699950f
Nicholas Archuletta <archulettanicholas@gmail.com>
2025-01-14 20:40:01 +0000

    finished templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'Nicholas'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Terry'},
+            'body': 'I got a new car!'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
